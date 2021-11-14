cca3f75c60b7576c1d7911932e56df35b7e86a23
ga1kokuj1n <maksimp.20040903@gmail.com>
2021-11-14 14:29:36 +0300

    3.1

--- Lab3.1/Lab3.1.cs
+++ Lab3.1/Lab3.1.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace Lab3._1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ulong n = 0;
+		t1:
+			Console.Write("Введите кол-во элементов массива: ");
+			try
+			{
+				n = ulong.Parse(Console.ReadLine());
+				if (n == 0)
+                {
+                    Console.WriteLine("Пожалуйста, введите положительное целое число!");
+					goto t1;
+                }
+			}
+            catch (FormatException)
+            {
+				Console.WriteLine("Пожалуйста, введите положительное целое число!");
+				goto t1;
+			}
+			catch (OverflowException)
+            {
+				Console.WriteLine("Это число слишком большое, пожалуйста, используйте другое");
+            }
+
+			int[] arr = new int[n];
+			Random rand = new Random();
+			int i = 0;
+            Console.WriteLine($"\nПолучившийся массив:\n");
+			foreach (int o in arr)
+            {
+				arr[i] = rand.Next(-30, 45);
+				Console.Write(arr[i] + ((i + 1)%10 == 0 ? "\n" : "\t"));
+				i++;
+			}
+            Console.WriteLine("\n\nВ обратном порядке без отрицательных\n");
+			i = (int)n - 1;
+			foreach (int o in arr)
+            {
+				if (arr[i] >= 0)
+					Console.Write(arr[i] + ", ");
+				i--;
+            }
+            Console.WriteLine("\b\b\0\0");
+		}
+	}
 }
---
+++ Lab3.2/Lab3.2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rand = new Random();
+			byte[,] arr = new byte[7, 7];
+			Console.WriteLine("Получившийся массив:\n");
+			for (byte i = 0; i < 7; i++)
+				for (byte j = 0; j < 7; j++)
+				{
+					arr[i, j] = (byte)rand.Next(10);
+					Console.Write(arr[i, j] + (j == 6 ? "\n" : " "));
+				}
+			byte c;
+			for (byte i = 0; i < 3; i++)
+				for (byte j = 0; j < 6 - i*2; j++)
+				{
+					c = arr[i, j + i];
+					arr[i, j + i] = arr[6 - j - i, i];
+					arr[6 - j - i, i] = arr[6 - i, 6 - j - i];
+					arr[6 - i, 6 - j - i] = arr[j + i, 6 - i];
+					arr[j + i, 6 - i] = c;
+				}
+			Console.WriteLine("\nПосле поворота на 90 градусов вправо:\n");
+			for (int i = 0; i < 7; i++)
+				for (int j = 0; j < 7; j++)
+					Console.Write(arr[i, j] + (j == 6 ? "\n" : " "));
+		}
+	}
+}
